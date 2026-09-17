11c00fcd40bfcad18f9ea791b0d7e8aeef3b17b6
yaakovizel <z7685727@gmail.com>
2026-09-17 12:12:18 +0300

    added routes in app

--- frontand/src/App.tsx
+++ frontand/src/App.tsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
